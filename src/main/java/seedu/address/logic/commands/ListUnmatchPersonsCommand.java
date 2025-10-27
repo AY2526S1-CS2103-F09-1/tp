462dc48d69c6353d8f2d1a1e130c56c4b4298fa7
BoredFurry <ian.derp.lim@gmail.com>
2025-10-28 00:01:03 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/ListUnmatchPersonsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListUnmatchPersonsCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
-import seedu.address.model.person.CentreContainsKeywordsPredicate;
 import seedu.address.model.person.MentorInCentreHasStudentsPredicate;
 
 /**
